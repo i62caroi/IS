79c9b4afe89545f34f6d84d35084f6de812a7712
Antonio Bogallo Fernandez <i72bofea@uco.es>
2018-11-27 17:14:43 +0100

    actualizacion codigo_añadir_alumnos

---
+++ Practica_4/codigoANADIRALUMNO.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include"codigo_ANADIR_ALUMNO.h"
+using namespace std;
+
+
+void Anadir_alumno::setLider(){
+
+	cout<<"Introduzca 'true' o '1' si el alumno es lider o 'false' o '0' si no lo es"<<endl;
+	cin>>lider_;
+
+}
