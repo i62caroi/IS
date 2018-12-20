8225a0d191e20d0bb214b2b448ca710234e4d83c
Antonio Bogallo Fernandez <i72bofea@uco.es>
2018-12-20 18:29:10 +0100

    cargar y guardar ficheros

--- Practica_4/Programa Final/Profesor.cc
+++ Practica_4/Programa Final/Profesor.cc
@@ -9,13 +9,86 @@ using namespace std;
 
 /*
 void Profesor::Cargar_fichero(list<Alumno>&lista){
-	// TODO - implement Profesor::Cargar_copia_seguridad
-	throw "Not yet implemented";
+
+	ifstream f;
+	list<Alumno>::iterator it;
+	Alumno p;
+	char nombre_fichero[30];
+
+	ifstream f;
+
+	cout<<"Introduzca el nombre del fichero binario que desea cargar"<<endl;
+	cin>>nombre_fichero;
+
+	f.open(nombre_fichero,ios::in | ios::binary);
+
+	while(!f.eof()){
+
+		f.read((char *)&p, sizeof(p));
+
+		lista.push_back(p);
+
+	}
+
+	f.close();
+
 }
 
 void Profesor::Guardar_fichero(list <Alumno> &lista){
-	// TODO - implement Profesor::Cargar_copia_seguridad
-	throw "Not yet implemented";
+
+	list<Alumno>::iterator it;
+	char aux[60];
+	int aux2;
+	Alumno p;
+
+	ofstream f;
+
+	cout<<"Introduzca el nombre del fichero de guardado"<<endl;
+	cin>>aux;
+
+	f.open(aux,ios::out | ios::binary);
+
+	for(it = lista.begin();it != lista.end();it++){
+
+		strcpy(aux,it->getNombre());
+		p.setNombre(aux);
+
+		strcpy(aux,it->getApellidos());
+		p.setApellidos(aux);
+
+		strcpy(aux,it->getDireccion());
+		p.setDireccion(aux);
+
+		strcpy(aux,it->getEmail());
+		p.setEmail(aux);
+
+		strcpy(aux,it->getDni());
+		p.setDni(aux);
+
+		strcpy(aux,it->getFecha_nacimiento());
+		p.setFecha_nacimiento(aux);
+
+		strcpy(aux,it->getNombre());
+		p.setNombre(aux);
+
+		aux2=it->getCurso();
+		p.setCurso(aux2);
+
+		aux2=it->getGrupo();
+		p.setGrupo(aux2);
+
+		aux2=it->getTelefono();
+		p.setTelefono(aux2);
+
+		strcpy(aux,it->getLider());
+		p.setLider(aux);
+
+		f.write((char *)&p, sizeof(p));
+
+	}
+
+	f.close();
+
 }
 
 void Profesor::Cargar_copia_seguridad(char tipo) {
